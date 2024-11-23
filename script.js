fa3fe44210a6adc29904fbfe92a72aab94e9d8d7
MHamed <aitsidiouali@gmail.com>
2024-11-23 19:44:21 +0100

    completed the console game and tested it and it's working, now it's time to restructure the code and make the UI works

--- script.js
+++ script.js
@@ -5,34 +5,21 @@ let board = [
         [6, 7, 8]
     ]
 
-function humanPlayer(){
-    this.marker = 'X'
+function Player(marker){
+    this.marker = marker
 }
 
-function botPlayer(){
-    this.marker = 'O'
-}
 
-function gameFlow(X_turn, O_turn){
-    this.X_turn = X_turn
-    this.O_turn = O_turn
-}
-
-function getMove(){
-    let valid_square = false
-    let val = null
-    let square = null
-    let availableSquares = availableSquares()
-    while (!valid_square){
-        square = prompt("Type in your square")
-        if (!isNaN(square)){
-            console.log("Invalid input! Try again")
-        } else if (!availableSquares.includes(square)){
-            console.log("Invalid Square! Try again")
+function availableSquares(board){
+    let squares = []
+    for (let i = 0; i < board.length; i++){
+        for (let j = 0; j < board[i].length; j++){
+            if (board[i][j] !== 'X' && board[i][j] !== 'O'){
+                squares.push(board[i][j])
+            }
         }
     }
-    val = square
-    return square
+    return squares
 }
 
 function displayBoard(board){
@@ -40,20 +27,93 @@ function displayBoard(board){
     console.log(board[i])
 }
 
-function availableSquares(){
-    let availableSquares = []
-    for (let i = 0; i < board.length; i++){
-        for (let j = 0; j < board.length; j++){
-            if (board[i][j] !== 'X' || board[i][j] !== 'O'){
-                availableSquares.push(board[i][j])
-            }
+function checkWinner(board){
+    //check rows
+    let winner = ''
+    for (let i=0; i<board.length; i++){
+        if (board[i][0] === board[i][1] && board[i][0] === board[i][2]){
+            winner = board[i][0]
+        }
+    }
+    //check columns
+    for (let i = 0; i<board.length; i++){
+        if (board[0][i] === board[1][i] && board[0][i] === board[2][i]){
+            winner = board[0][i]
+        }
+    }
+    //check the diagonals
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2]){
+        winner = board[0][0]
+    } else if (board[0][2] === board[1][1] && board[1][1] === board[2][0]){
+        winner = board[0][2]
+    }
+    return winner
+}
+
+function getMove(board){
+    let valid_square = false
+    let squares = availableSquares(board)
+    let square;
+    while (!valid_square){
+        square = Math.floor(prompt("Type in your square"))
+        if (isNaN(square)){
+            console.log("Invalid input! Try again")
+        } else if (!squares.includes(square)){
+            console.log("Invalid Square! Try again")
+        } else {
+            valid_square = true
+        }
+    }
+    return square
+}
+
+function placeMove(player, board){
+    let square = getMove(board)
+    if (square <= 2){
+        const i = board[0].indexOf(square)
+        board[0][i] = player.marker
+    } else if (square > 2 && square <= 5){
+        const i = board[1].indexOf(square)
+        board[1][i] = player.marker
+    } else if (square > 5){
+        const i = board[2].indexOf(square)
+        board[2][i] = player.marker
+    }
+}
+
+function gameController() {
+    let humanPlayer = new Player('X')
+    let botPlayer = new Player('O')
+    let isOver = false
+    while (isOver == false){
+        console.log("X's turn")
+        placeMove(humanPlayer, board)
+        displayBoard(board)
+        if (checkWinner(board) === 'X'){
+            console.log("X Player wins")
+            isOver = true
+            break
+        } else if (compareFunc(availableSquares(board), [])){
+            console.log("It's a Draw")
+            isOver = true
+            break
+        }
+
+        console.log("O's turn")
+        placeMove(botPlayer, board)
+        displayBoard(board)
+        if (checkWinner(board) === 'X'){
+            console.log("X Player wins")
+            isOver = true
+            break
+        } else if (compareFunc(availableSquares(board), [])){
+            console.log("It's a Draw")
+            isOver = true
+            break
         }
     }
 }
 
-function checkWinner(){
-
-}
-
-function playGame() {
-}
+const compareFunc = (a, b) =>
+    a.length === b.length &&
+    a.every((element, index) => element === b[index]);
